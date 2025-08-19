d4cfb8b2ddd115cb4f2ba857fbf55772044d201f
Pawel Jaworski <pawel.jaworski-extern@dbschenker.com>
2025-08-19 15:34:28 +0200

    test utils

--- src/test/java/pl/pjaworski/jdgtaxes/testutil/TestDataExamples.java
+++ src/test/java/pl/pjaworski/jdgtaxes/testutil/TestDataExamples.java
@@ -1,8 +1,10 @@
 package pl.pjaworski.jdgtaxes.testutil;
 
+import java.time.LocalDate;
 import java.time.YearMonth;
 
 public interface TestDataExamples {
     String NIP = "123-456-32-18";
-    YearMonth SEPTEMBER = YearMonth.parse("2025-09");
+    LocalDate SEP_01_2025 = LocalDate.parse("2025-09-01");
+    YearMonth SEP_2025 = YearMonth.parse("2025-09");
 }
